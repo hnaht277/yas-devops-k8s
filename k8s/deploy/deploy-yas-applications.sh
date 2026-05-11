eb5465606de7d8db310bb07d31c72facb5233527
TMQ2k <kentran072009@gmail.com>
2026-05-11 23:58:27 +0700

    feat: add deployment script for yas applications using Helm

--- k8s/deploy/deploy-yas-applications.sh
+++ k8s/deploy/deploy-yas-applications.sh
@@ -10,36 +10,36 @@ read -rd '' DOMAIN \
 
 helm dependency build ../charts/backoffice-bff
 helm upgrade --install backoffice-bff ../charts/backoffice-bff \
---namespace yas --create-namespace \
+--namespace yas-dev --create-namespace \
 --set backend.ingress.host="backoffice.$DOMAIN"
 
 helm dependency build ../charts/backoffice-ui
 helm upgrade --install backoffice-ui ../charts/backoffice-ui \
---namespace yas --create-namespace
+--namespace yas-dev --create-namespace
 
 sleep 60
 
 helm dependency build ../charts/storefront-bff
 helm upgrade --install storefront-bff ../charts/storefront-bff \
---namespace yas --create-namespace \
+--namespace yas-dev --create-namespace \
 --set backend.ingress.host="storefront.$DOMAIN"
 
 helm dependency build ../charts/storefront-ui
 helm upgrade --install storefront-ui ../charts/storefront-ui \
---namespace yas --create-namespace
+--namespace yas-dev --create-namespace
 
 sleep 60
 
 helm upgrade --install swagger-ui ../charts/swagger-ui \
---namespace yas --create-namespace \
+--namespace yas-dev --create-namespace \
 --set ingress.host="api.$DOMAIN"
 
 sleep 20
 
-for chart in {"cart","customer","inventory","location","media","order","payment","payment-paypal","product","promotion","rating","search","tax","recommendation","webhook","sampledata"} ; do
+for chart in {"cart","customer","inventory","location","media","order","payment","product","promotion","rating","search","tax","recommendation","webhook","sampledata"} ; do
     helm dependency build ../charts/"$chart"
     helm upgrade --install "$chart" ../charts/"$chart" \
-    --namespace yas --create-namespace \
+    --namespace yas-dev --create-namespace \
     --set backend.ingress.host="api.$DOMAIN"
     sleep 60
 done
